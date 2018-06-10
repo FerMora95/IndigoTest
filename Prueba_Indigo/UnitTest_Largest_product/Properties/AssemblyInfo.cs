36061c45367968cda3984bfe4b6389abea0df637
FerMora95 <femogonino@gmail.com>
2018-06-10 02:13:27 -0500

    Add files via upload

---
+++ Prueba_Indigo/UnitTest_Largest_product/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest_Largest_product")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest_Largest_product")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5033b1d7-a7e7-4c0e-a214-3927b1463846")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Prueba_Indigo/UnitTest_Largest_product/Test_Largest_product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Largest_product_in_a_series;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest_Largest_product
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            try
+            {
+                var serie = new Series();
+                string valores = "";
+                string[] Val = new string[2];
+                valores = "10 5";
+                Val = valores.Split(' ');
+                serie.L = Convert.ToInt32(Val[0]);
+                serie.K = Convert.ToInt32(Val[1]);
+
+                serie.N = Convert.ToInt32("3675356291");
+                serie.Serie();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
