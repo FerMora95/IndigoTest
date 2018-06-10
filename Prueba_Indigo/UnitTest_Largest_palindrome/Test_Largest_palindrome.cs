da7915d4b5bbeef44fc8d83b2d6d69765eddb34b
FerMora95 <femogonino@gmail.com>
2018-06-10 02:10:34 -0500

    Add files via upload

---
+++ Prueba_Indigo/UnitTest_Largest_palindrome/Test_Largest_palindrome.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Largest_palindrome_product;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PruebasUnitarias
+{
+    [TestClass]
+    public class Test_Largest_palindrome
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var palindromo = new Palindrome();
+            try
+            {
+                palindromo.N = Convert.ToInt32("800000");
+                palindromo.Palindromo();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
