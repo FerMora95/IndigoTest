4213a71286aff4c41848c9d4f37bf9b47dace40b
FerMora95 <femogonino@gmail.com>
2018-06-10 01:30:38 -0500

    Add files via upload

---
+++ Prueba_Indigo/ConsoleApp1/Series.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_product_in_a_series
+{
+    public class Series
+    {
+
+        public int K { get { return K; }
+            set
+            {
+                if ((value >= 1) && (value <= 7)) { K = value; } else { K = 0; } }
+                     }
+        public int N { get { return N; }
+                        set { if ((value >= K) && (value <= 1000)) { N = value; } else { N = 0; } }
+                     }
+        public int T { get { return T; }
+                        set { if ((value >= 1) && (value <= 100)) { T = value; } else { T = 0; } }
+                     }
+
+        public int L {get { return L; }
+                            set { if ((value >= K) && (value <= 1000)) { L = value; } else { L = 0; } }
+        }
+        static void Main(string[] args)
+        {
+        }
+
+        public void Serie()
+        {
+            if ( K == 0) { Console.WriteLine("The variable 'K' is outside the established ranges"); }
+            else if (N == 0) { Console.WriteLine("The variable 'N' is outside the established ranges");}
+            else if (N == 0) { Console.WriteLine("The variable 'T' is outside the established ranges"); }
+            else { 
+                string Num = this.N.ToString();
+                string[] Nums= new string[0];            
+                for (int i = 1; (i + this.K) <= this.L; i++)
+                    {
+                    Array.Resize(ref Nums, Nums.Length + i);
+                    Nums[i-1] = Num.Substring(i, this.K);
+                    }
+                int x=1;
+                foreach (string item in Nums)
+                {
+                    x = Convert.ToInt32(item.Substring(1, 1)) * x; 
+                }
+                Console.WriteLine(x.ToString());
+            }
+        }
+
+    }
+}
---
+++ Prueba_Indigo/Largest_palindrome_product/Palindrome.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Largest_palindrome_product
+{
+    public class Palindrome
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public int T { get; set; }                
+        public int N
+        {
+            get { return N; }
+            set { if ((101101 < value) && (value < 1000000)) { N = value; } else { N = 0; } }
+        }
+
+        public void Palindromo()
+        {
+            if (this.N == 0)
+            {
+                Console.WriteLine("The variable 'N' is outside the established ranges");
+            }else { 
+                int p = 0;
+                string[] Palindromo = new string[1];
+                for (int x = 999; x >= 100; x--)
+                {
+                    for (int y = 999; y >= 100; y--)
+                    {
+                        p = (x * y);
+
+                        if (p == Convert.ToInt32(ReverseString(p.ToString())))
+                        {
+                            if (p <= this.N)
+                            {
+                                if (Convert.ToInt32(Palindromo[0]) < Convert.ToInt32(ReverseString(p.ToString())))
+                                {
+                                    Palindromo[0] = ReverseString(p.ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+                Console.WriteLine(Palindromo[0]);
+            }
+        }
+
+        public static string ReverseString(string s)
+        {
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+    }
+}
